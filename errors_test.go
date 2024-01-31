3b22e41775156026cc1417fe25d5e824ed8f3939
Eugene Sumin <95425330+e-sumin@users.noreply.github.com>
2024-01-31 11:28:15 +0100

    Draft 2 (#3)

--- errors_test.go
+++ errors_test.go
@@ -27,9 +27,9 @@ func newTestError(msg string) *testErrorType {
 }
 
 var (
-	predefinedStdError    = errors.New("TEST_ERR: Sample of predefined std error")
-	predefinedErrkitError = errkit.NewPureError("TEST_ERR: Sample of errkit error")
-	predefinedTestError   = newTestError("TEST_ERR: Sample error of custom test type")
+	predefinedStdError  = errors.New("TEST_ERR: Sample of predefined std error")
+	predefinedPureError = errkit.NewPureError("TEST_ERR: Sample of pure error")
+	predefinedTestError = newTestError("TEST_ERR: Sample error of custom test type")
 )
 
 type Check func(originalErr error, jsonErr errkit.JSONError) error
@@ -38,6 +38,15 @@ func getMessageCheck(msg string) Check {
 	return func(_ error, err errkit.JSONError) error {
 		if err.Message != msg {
 			return fmt.Errorf("error message does not match the expectd\nexpected: %s\nactual: %s", msg, err.Message)
+		}
+		return nil
+	}
+}
+
+func getTextCheck(msg string) Check {
+	return func(origError error, _ errkit.JSONError) error {
+		if origError.Error() != msg {
+			return fmt.Errorf("error text does not match the expected\nexpected: %s\nactual: %s", msg, origError.Error())
 		}
 		return nil
 	}
@@ -110,7 +119,11 @@ func getDetailsCheck(details errkit.ErrorDetails) Check {
 func checkErrorResult(t *testing.T, err error, checks ...Check) {
 	t.Helper()
 
-	got := err.Error()
+	got, e := json.Marshal(err)
+	if e != nil {
+		t.Errorf("Error marshaling failed: %s", e.Error())
+		return
+	}
 
 	var unmarshalledError errkit.JSONError
 	unmarshallingErr := unmarshalledError.UnmarshalJSON([]byte(got))
@@ -130,7 +143,7 @@ func checkErrorResult(t *testing.T, err error, checks ...Check) {
 
 func TestErrorCreation(t *testing.T) {
 	t.Run("It should be possible to create pure errors which could be used as named errors", func(t *testing.T) {
-		e := predefinedErrkitError.Error()
+		e := predefinedPureError.Error()
 		if e != "TEST_ERR: Sample of errkit error" {
 			t.Errorf("Unexpected result")
 		}
@@ -141,19 +154,32 @@ func TestErrorsWrapping(t *testing.T) {
 	t.Run("It should be possible to wrap std error, which should be stored as cause", func(t *testing.T) {
 		wrappedStdError := errkit.Wrap(predefinedStdError, "Wrapped STD error")
 		checkErrorResult(t, wrappedStdError,
-			getMessageCheck("Wrapped STD error"), // Checking what msg is serialized on top level
+			getMessageCheck("Wrapped STD error"),                                        // Checking what msg is serialized on top level
+			getTextCheck("Wrapped STD error: TEST_ERR: Sample of predefined std error"), // Checking what error message is generated
 			filenameCheck,                        // Checking callstack capture
 			getErrkitIsCheck(predefinedStdError), // Checking that original error was successfully wrapped
 			getUnwrapCheck(predefinedStdError),   // Checking that it's possible to unwrap wrapped error
 		)
 	})
 
-	t.Run("It should be possible to wrap errkit error, which should be stored as cause", func(t *testing.T) {
-		wrappedErrkitError := errkit.Wrap(predefinedErrkitError, "Wrapped errkit error")
+	t.Run("It should be possible to wrap errkit pure error, which should be stored as cause", func(t *testing.T) {
+		wrappedErrkitError := errkit.Wrap(predefinedPureError, "Wrapped errkit error")
 		checkErrorResult(t, wrappedErrkitError,
 			getMessageCheck("Wrapped errkit error"), // Checking what msg is serialized on top level
-			filenameCheck,                           // Checking callstack capture
-			getErrkitIsCheck(predefinedErrkitError), // Checking that original error was successfully wrapped
+			getTextCheck("Wrapped errkit error: TEST_ERR: Sample of pure error"),
+			filenameCheck,                         // Checking callstack capture
+			getErrkitIsCheck(predefinedPureError), // Checking that original error was successfully wrapped
+		)
+	})
+
+	t.Run("It should be possible to wrap errkit error, which should be stored as cause", func(t *testing.T) {
+		someErrkitError := errkit.New("Original errkit error")
+		wrappedErrkitError := errkit.Wrap(someErrkitError, "Wrapped errkit error")
+		checkErrorResult(t, wrappedErrkitError,
+			getMessageCheck("Wrapped errkit error"), // Checking what msg is serialized on top level
+			getTextCheck("Wrapped errkit error: Original errkit error"),
+			filenameCheck,                     // Checking callstack capture
+			getErrkitIsCheck(someErrkitError), // Checking that original error was successfully wrapped
 		)
 	})
 
@@ -183,6 +209,7 @@ func TestErrorsWrapping(t *testing.T) {
 		wrappedErr := errkit.WithCause(errorNotFound, cause)
 		checkErrorResult(t, wrappedErr,
 			getMessageCheck("Resource not found"), // Check top level msg
+			getTextCheck("Resource not found: Reason why resource not found"),
 			filenameCheck,
 			getErrkitIsCheck(cause),         // Check that cause was properly wrapped
 			getErrkitIsCheck(errorNotFound), // Check that predefined error is also matchable
@@ -211,15 +238,9 @@ func TestErrorsWrapping(t *testing.T) {
 		checkErrorResult(t, wrappedErr,
 			getMessageCheck("Resource not found"), // Check top level msg
 			filenameCheck,
-			getErrkitIsCheck(cause), // Check that cause was properly wrapped
-			getUnwrapCheck(cause),   // Check that unwrapping of error returns cause
-			func(origErr error, jsonErr errkit.JSONError) error { // Check that only message was taken from the errorNotFound
-				if errkit.Is(origErr, errorNotFound) {
-					return errors.New("error is implementing unexpected type")
-				}
-
-				return nil
-			},
+			getErrkitIsCheck(cause),         // Check that cause was properly wrapped
+			getErrkitIsCheck(errorNotFound), // Check that predefined error is also matchable
+			getUnwrapCheck(cause),           // Check that unwrapping of error returns cause
 		)
 	})
 
@@ -239,11 +260,12 @@ func TestErrorsWithDetails(t *testing.T) {
 	// Expecting the following JSON (except stack) for special case
 	oddResult := "{\"message\":\"Some error with details\",\"details\":{\"Some numeric detail\":\"NOVAL\",\"Some text detail\":\"String value\"}}"
 	invalidKeyResult := "{\"message\":\"Some error with details\",\"details\":{\"BADKEY:(123)\":456,\"Some text detail\":\"String value\"}}"
-	wrappedResult := "{\"message\":\"Wrapped error\",\"details\":{\"Some numeric detail\":123,\"Some text detail\":\"String value\"},\"cause\":{\"message\":\"TEST_ERR: Sample of errkit error\"}}"
+	wrappedResult := "{\"message\":\"Wrapped error\",\"details\":{\"Some numeric detail\":123,\"Some text detail\":\"String value\"},\"cause\":{\"message\":\"TEST_ERR: Sample of pure error\"}}"
 
 	getResultCheck := func(expected string) Check {
 		return func(orig error, _ errkit.JSONError) error {
-			errStr := orig.Error()
+			b, _ := json.Marshal(orig)
+			errStr := string(b)
 			type simplifiedStruct struct {
 				Message string            `json:"message"`
 				Details map[string]any    `json:"details,omitempty"`
@@ -279,12 +301,12 @@ func TestErrorsWithDetails(t *testing.T) {
 	})
 
 	t.Run("It should be possible to wrap an error and add details at once", func(t *testing.T) {
-		err := errkit.Wrap(predefinedErrkitError, "Wrapped error", "Some text detail", "String value", "Some numeric detail", 123)
+		err := errkit.Wrap(predefinedPureError, "Wrapped error", "Some text detail", "String value", "Some numeric detail", 123)
 		checkErrorResult(t, err, getResultCheck(wrappedResult))
 	})
 
 	t.Run("It should be possible to wrap an error and add details at once using ErrorDetails map", func(t *testing.T) {
-		err := errkit.Wrap(predefinedErrkitError, "Wrapped error", errkit.ErrorDetails{"Some text detail": "String value", "Some numeric detail": 123})
+		err := errkit.Wrap(predefinedPureError, "Wrapped error", errkit.ErrorDetails{"Some text detail": "String value", "Some numeric detail": 123})
 		checkErrorResult(t, err, getResultCheck(wrappedResult))
 	})
 
@@ -329,8 +351,8 @@ func TestErrorsWithStack(t *testing.T) {
 		checkErrorResult(t, err,
 			getMessageCheck("Resource not found"), // Check top level msg
 			getStackCheck(fnName, lineNumber+1),
-			getErrkitIsCheck(errorNotFound), // Check that errorNotWanted is still matchable
-			getUnwrapCheck(errorNotFound),   // Check that we are able to unwrap original error
+			getErrkitIsCheck(errorNotFound), // Check that errorNotFound is still matchable
+			getUnwrapCheck(nil),             // Check that we are able to unwrap original error
 		)
 	})
 
@@ -386,11 +408,11 @@ func TestMultipleErrors(t *testing.T) {
 
 	t.Run("It should be possible to append multiple errkit.errkitError to errors list", func(t *testing.T) {
 		someErr := errkit.New("Some test error")
-		err := errkit.Append(predefinedErrkitError, someErr)
+		err := errkit.Append(predefinedPureError, someErr)
 		str := err.Error()
 
 		someErrStr := someErr.Error()
-		predefinedErrStr := predefinedErrkitError.Error()
+		predefinedErrStr := predefinedPureError.Error()
 
 		arr := append(make([]string, 0), predefinedErrStr, someErrStr)
 		arrStr, _ := json.Marshal(arr)
---
+++ marshable_error.go
@@ -0,0 +1,129 @@
+package errkit
+
+import (
+	"encoding"
+	"encoding/json"
+	"runtime"
+	"strings"
+)
+
+func getLocationFromStack(stack []uintptr, callers int) (function, file string, line int) {
+	if callers < 1 {
+		// Failure potentially due to wrongly specified depth
+		return "Unknown", "Unknown", 0
+	}
+
+	frames := runtime.CallersFrames(stack[:callers])
+	var frame runtime.Frame
+	frame, _ = frames.Next()
+	filename := frame.File
+	if paths := strings.SplitAfterN(frame.File, "/go/src/", 2); len(paths) > 1 {
+		filename = paths[1]
+	}
+
+	return frame.Function, filename, frame.Line
+}
+
+type JSONError struct {
+	Message    string       `json:"message,omitempty"`
+	Function   string       `json:"function,omitempty"`
+	LineNumber int          `json:"linenumber,omitempty"`
+	File       string       `json:"file,omitempty"`
+	Details    ErrorDetails `json:"details,omitempty"`
+	Cause      any          `json:"cause,omitempty"`
+}
+
+// JSONMarshable attempts to produce a JSON representation of the given err.
+// If the resulting string is empty, then the JSON encoding of the err.Error()
+// string is returned or empty if the Error() string cannot be encoded.
+func JSONMarshable(err error) any {
+	if err == nil {
+		return nil
+	}
+
+	switch err.(type) {
+	case json.Marshaler, encoding.TextMarshaler:
+		return err
+	default:
+		// Otherwise wrap the error with {"message":"…"}
+		return JSONError{Message: err.Error()}
+	}
+}
+
+type MarshableErrorList []JSONError
+
+func MarshalErrkitErrorToJSON(err *errkitError) ([]byte, error) {
+	if err == nil {
+		return nil, nil
+	}
+
+	function, file, line := getLocationFromStack(err.stack, err.callers)
+
+	result := JSONError{
+		Message:    err.Message(),
+		Function:   function,
+		LineNumber: line,
+		File:       file,
+		Details:    err.Details(),
+	}
+
+	if err.cause != nil {
+		if kerr, ok := err.cause.(*errkitError); ok {
+			causeJSON, err := MarshalErrkitErrorToJSON(kerr)
+			if err != nil {
+				return nil, err
+			}
+
+			result.Cause = json.RawMessage(causeJSON)
+		} else {
+			result.Cause = JSONMarshable(err.cause)
+		}
+	}
+
+	return json.Marshal(result)
+}
+
+// jsonError is a data structure which helps to deserialize error at once.
+type jsonError struct {
+	Message    string          `json:"message,omitempty"`
+	Function   string          `json:"function,omitempty"`
+	LineNumber int             `json:"linenumber,omitempty"`
+	File       string          `json:"file,omitempty"`
+	Details    ErrorDetails    `json:"details,omitempty"`
+	Cause      json.RawMessage `json:"cause,omitempty"`
+}
+
+// UnmarshalJSON return error unmarshaled into JSONError.
+func (e *JSONError) UnmarshalJSON(source []byte) error {
+	var parsedError *jsonError
+	err := json.Unmarshal(source, &parsedError)
+	if err != nil {
+		return err
+	}
+
+	e.Message = parsedError.Message
+	e.Function = parsedError.Function
+	e.File = parsedError.File
+	e.LineNumber = parsedError.LineNumber
+	e.Details = parsedError.Details
+
+	if parsedError.Cause == nil {
+		return nil
+	}
+
+	// Trying to parse as JSONError
+	var jsonErrorCause *JSONError
+	err = json.Unmarshal(parsedError.Cause, &jsonErrorCause)
+	if err == nil {
+		e.Cause = jsonErrorCause
+		return nil
+	}
+
+	//  fallback to any
+	var cause any
+	err = json.Unmarshal(parsedError.Cause, &cause)
+	if err == nil {
+		e.Cause = cause
+	}
+	return err
+}
